913ff877ed9eabe6b88987c82990de25905c7e01
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2023-10-26 15:58:16 +0000

    feat: Finish sqlite3 example

--- coding/coding/python/sql_practice/main.py
+++ coding/coding/python/sql_practice/main.py
@@ -1,13 +1,16 @@
 import sqlite3
 
 
+# Creates the db if it doesn't exist
 connection = sqlite3.connect("database.db")
-cursor = connection.cursor()
+
 # Cursor is in charge of executing SQL queries/cmds
+cursor = connection.cursor()
 
-cursor.execute("CREATE TABLE students (name TEXT, age INTEGER, score INTEGER)")
 # string = TEXT, float = REAL, int = INTEGER, binary = BLOB
+cursor.execute("CREATE TABLE students (name TEXT, age INTEGER, score INTEGER)")
 
+# List of tuples containing the table information to be stored
 students = [
     ("Greg", 14, 98),
     ("Tom", 13, 77),
@@ -15,10 +18,13 @@ students = [
     ("Lisa", 14, 92),
     ("Jen", 15, 66),
 ]
+
 # Since it's not just 1 item being inserted, we need to use `insertmany()`
 cursor.executemany("INSERT INTO students VALUES (?,?,?)", students)
 
+# Grab all the items in table `students`
 for row in cursor.execute('SELECT * FROM students'):
     print(row)
 
+# Must close the connection at the end.
 connection.close()
