f37906b18d8b3e758c1c91ad51394927a92b51bc
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2023-12-14 14:21:13 -0500

    refactor: Clean up sourcegraph code in after directory

--- nvim/.config/nvim/after/plugin/sourcegraph.lua
+++ nvim/.config/nvim/after/plugin/sourcegraph.lua
@@ -1,75 +1,57 @@
 local sg = require('sg')
 local sgui = require('sg.extensions.telescope')
 local os = require('kolkhis.detect_os')
--- Sourcegraph configuration. All keys are optional
+-- Sourcegraph config. All keys are optional
 if os.is_linux then
   sg.setup({})
   vim.keymap.set('n', '<leader>ss', function()
-    -- "<cmd>lua require('sg.extensions.telescope').fuzzy_search_results()<CR>"
     sgui.fuzzy_search_results()
   end)
-  ---Prompts user to ask Cody about the current selection
-  local function ask_cody()
-    vim.ui.input({ prompt = 'Ask Cody > ' }, function(question)
-      vim.cmd((":'<,'>CodyAsk %s"):format(question))
-    end)
-  end
 
-  local function cody_task()
-    local mode = vim.api.nvim_get_mode()
-    if mode == 'n' then
-      vim.ui.input({ prompt = 'Cody Task (global) > ' }, function(task)
-        vim.cmd((':%CodyTask %s'):format(task))
-      end)
-    else
-      vim.ui.input({ prompt = 'Cody Task > ' }, function(task)
-        vim.cmd((":'<,'>CodyTask %s"):format(task))
-      end)
-    end
-  end
-  -- -- TODO: Get prompt
-  -- vim.keymap.set('v', '<leader>ca', ask_cody, { noremap = true, desc = "Ask Cody about the current selection" })
-
-  vim.keymap.set({ 'n', 'v' }, '<leader>ca', function()
+  local cody_ask = function()
     local mode = vim.api.nvim_get_mode().mode
-    if mode == 'n' then
-      vim.ui.input({ prompt = 'Ask Cody about file > ' }, function(question)
+    local prompt = mode == 'n' and 'Ask Cody (file) > ' or 'Ask Cody (selection) > '
+    vim.ui.input({ prompt = prompt }, function(question)
+      if mode == 'n' then
         vim.cmd((':%% CodyAsk %s'):format(question))
-      end)
-    else
-      vim.ui.input({ prompt = 'Ask Cody about selection > ' }, function(question)
+      else
         vim.cmd.norm('I')
         vim.cmd(("'<,'>CodyAsk %s"):format(question))
-      end)
-    end
-  end)
-
-  vim.keymap.set(
-    { 'n', 'v' },
-    '<leader>co',
-    '<cmd>CodyToggle<CR>',
-    { silent = true, noremap = true, desc = 'Toggle the Cody Chat Window' }
-  )
-
-  vim.keymap.set({ 'n', 'v' }, '<leader>cp', function()
-    vim.ui.input({ prompt = 'Cody Task -> ' }, function(task)
-      vim.cmd(('CodyTask %s'):format(task))
+      end
     end)
-  end)
+  end
 
-  vim.keymap.set({ 'n', 'v' }, '<leader>cp', function()
-    local mode = vim.api.nvim_get_mode().mode
-    if mode == 'n' then
-      vim.ui.input({ prompt = 'Cody Task (global) > ' }, function(task)
-        vim.cmd(('%% CodyTask %s'):format(task))
-      end)
-    else
-      vim.ui.input({ prompt = 'Cody Task > ' }, function(task)
-        vim.cmd.norm('I')
-        vim.cmd((":'<,'>CodyTask %s"):format(task))
-      end)
-    end
-  end, { noremap = true, desc = 'Task [C]ody with a [P]rompt.' })
+   local cody_task = function()
+     local mode = vim.api.nvim_get_mode().mode
+     local prompt = mode == 'n' and 'Cody Task (file) > ' or 'Cody Task (selection) > '
+     vim.ui.input({ prompt = prompt }, function(task)
+       if mode == 'n' then
+         vim.cmd(('%% CodyTask %s'):format(task))
+       else
+         vim.cmd.norm('I')
+         vim.cmd((":'<,'>CodyTask %s"):format(task))
+       end
+     end)
+   end
+
+   vim.keymap.set(
+     { 'n', 'v' },
+     '<leader>co',
+     '<cmd>CodyToggle<CR>',
+     { silent = true, noremap = true, desc = 'Toggle the [Co]dy Chat Window' }
+   )
+
+   vim.keymap.set({ 'n', 'v' }, '<leader>ca', cody_ask, {
+     silent = true,
+     noremap = true,
+     desc = 'Get [C]ody to [A]nswer a question about the file or selection',
+   })
+
+   vim.keymap.set({ 'n', 'v' }, '<leader>cp', cody_task, {
+     silent = true,
+     noremap = true,
+     desc = 'Task [C]ody with a [P]rompt about file or selection.',
+   })
+
 end
 
--- End of os.is_linux() check
