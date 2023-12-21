3465912e2812a4c79fbad699bfe20f52c8dac3dc
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2023-12-20 20:03:50 -0500

    feat: Overhaul telescope config

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,90 +1,179 @@
+-- telescope.defaults.layout_config / layout_strategy / telescope.actions
+
+local actions = require('telescope.actions')
+local builtin = require('telescope.builtin')
+local themes = require('telescope.themes')
+local utils = require('telescope.utils')
 require('telescope').setup({
     defaults = {
+        layout_strategy = 'vertical',
+        layout_config = { height = 0.5 },
         mappings = {
             i = {
                 ['<C-u>'] = false,
                 ['<C-d>'] = false,
+                ['<C-p>'] = actions.move_selection_previous,
+                ['<C-n>'] = actions.move_selection_next,
+                ['<C-k>'] = actions.cycle_history_prev,
+                ['<C-j>'] = actions.cycle_history_next,
+                ['<C-f>'] =  actions.send_selected_to_qflist, -- actions.smart_add_to_qflist,
+            },
+
+            n = {
+                ['o'] = actions.file_vsplit,
+                ['<Tab>'] = actions.toggle_selection,
+                ['<C-d>'] = actions.preview_scrolling_down,
+                ['<C-u>'] = actions.preview_scrolling_up,
+                ['<C-p>'] = actions.move_selection_previous,
+                ['<C-n>'] = actions.move_selection_next,
+                ['<C-f>'] =  actions.send_selected_to_qflist, -- actions.smart_add_to_qflist,
             },
         },
     },
 })
-local builtin = require('telescope.builtin')
-local themes = require('telescope.themes')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = '<leader>pf [P]arse [F]iles' })
-vim.keymap.set('n', '<leader>pg', builtin.git_files, { desc = '<leader>pg [P]arse [G]it Files' })
-vim.keymap.set('n', '<leader>su', builtin.grep_string, { desc = '[S]earch [U]sages of Word' })
-vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc = '[P]arse [S]tring by Grep' })
-vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+
+-- -- Need to prepend 'lua.' to get autocompletion (y tho).
+-- local actions = require('lua.telescope.actions')
+-- local builtin = require('lua.telescope.builtin')
+-- local themes = require('lua.telescope.themes')
+-- local telescope = require('lua.telescope').setup({})
+-- local utils = require('lua.telescope.utils')
+
+--------------------------Seach Current Word Usage
+vim.keymap.set('n', '<leader>su', function()
+    builtin.grep_string(themes.get_dropdown({
+        cwd = utils.buffer_dir(),
+    }))
+end, { silent = true, noremap = true, desc = '[S]earch [U]sages of Current Word' })
+
+--------------------------Diagnostics
+vim.keymap.set('n', '<leader>sd', function()
+    builtin.diagnostics(themes.get_dropdown())
+end, { silent = true, noremap = true, desc = '[S]earch [D]iagnostics' })
+
+--------------------------Old Files
+vim.keymap.set('n', '<leader>of', function()
+    builtin.oldfiles(themes.get_dropdown())
+end, { silent = true, noremap = true, desc = '[?] Find Recently [O]pened [F]iles' })
+
+--------------------------Open Buffers
+vim.keymap.set('n', '<leader><space>', function()
+    builtin.buffers(themes.get_dropdown())
+end, { silent = true, noremap = true, desc = '[ ] Find existing buffers' })
+
+--------------------------Current Buffer Fuzzy Find
 vim.keymap.set('n', '<leader>/', function()
-    builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown({ winblend = 5, previewer = false }))
-end, { desc = '[/] Fuzzily search in current buffer' })
-vim.keymap.set('n', '<leader>rs', builtin.registers, { desc = 'Vim [R]egister [S]earch' })
-vim.keymap.set({ 'n', 'v' }, '<leader>ch', builtin.command_history, { desc = '[C]ommand [H]istory' })
+    builtin.current_buffer_fuzzy_find(themes.get_dropdown({ winblend = 5, previewer = false }))
+end, { silent = true, noremap = true, desc = '[/] Fuzzily search in current buffer' })
 
--- vim.keymap.set({'n'}, '<leader>qf', builtin.quickfix, { desc = '[Q]uick[f]ix'})
--- vim.keymap.set('n', '<leader>ma', builtin.man_pages, { desc = '[Ma]n Pages'})
+--------------------------Command History
+vim.keymap.set({ 'n', 'v' }, '<leader>ch', function()
+    builtin.command_history(themes.get_dropdown())
+end, { silent = true, noremap = true, desc = '[C]ommand [H]istory' })
 
--- builtin.current_buffer_fuzzy_find, { desc = '[/] Fuzzily search in current buffer' })
--- function()
---     vim.cmd('Telescope current_buffer_fuzzy_find')
--- end, { desc = '[/] Fuzzily search in current buffer' })
+--------------------------Registers
+vim.keymap.set('n', '<leader>rs', function()
+    builtin.registers(themes.get_cursor())
+end, { silent = true, noremap = true, desc = 'Vim [R]egister [S]earch' })
+
+--------------------------Quickfix
+vim.keymap.set({ 'n' }, '<leader>qf', function()
+    builtin.quickfix(themes.get_dropdown())
+end, { silent = true, noremap = true, desc = '[Q]uick[f]ix' })
+
+--------------------------Man Pages
+vim.keymap.set('n', '<leader>ma', function()
+    builtin.man_pages(themes.get_ivy())
+end, { silent = true, noremap = true, desc = '[Ma]n Pages' })
+
+--------------------------Search Help
+vim.keymap.set('n', '<leader>sh', function()
+    builtin.help_tags(themes.get_ivy({ winblend = 5 }))
+end, { silent = true, noremap = true, desc = '[S]earch [H]elp Tags' })
 
 vim.keymap.set('n', '<leader>hw', function()
-    vim.cmd('normal! yiw')
     local word = vim.fn.expand('<cword>')
-    builtin.help_tags(themes.get_ivy({ winblend = 5, search = word }))
-    -- vim.cmd(('normal! i%s'):format(vim.fn.expand('<cword>')))  -- This successfully inputs text into the search bar, but only the literal '<cWORD> <Esc>)'
-    -- vim.api.nvim_put({word}, 'c', true, false)
-    -- vim.api.nvim_put
-end, { desc = 'Search [H]elp Tags for [W]ord' })
+    builtin.help_tags(themes.get_ivy({ winblend = 2 }))
+    vim.cmd(('normal! i%s'):format(vim.fn.expand('<cword>')))
+    vim.api.nvim_put({ word }, 'c', true, false)
+end, { silent = true, noremap = true, desc = 'Search [H]elp Tags for [W]ord' })
 
+vim.keymap.set('n', '<leader>hW', function()
+    local word = vim.fn.expand('<cWORD>')
+    builtin.help_tags(themes.get_ivy({ winblend = 2 }))
+    vim.cmd(('normal! i%s'):format(vim.fn.expand('<cWORD>')))
+    vim.api.nvim_put({ word }, 'c', true, false)
+end, { silent = true, noremap = true, desc = 'Search [H]elp Tags for [W]ord' })
 --------------------------
 
-vim.keymap.set('n', '<leader>sh', function()
-    local word = vim.fn.expand('<cword>')
-    builtin.help_tags(themes.get_ivy({ winblend = 5, search = word }))
-end, { desc = '[S]earch [H]elp Tags' })
+--------------------------All Telescope Builtin Pickers
+vim.keymap.set('n', '<leader>pp', function()
+    builtin.builtin(themes.get_dropdown())
+end, { silent = true, noremap = true, desc = 'Telesco[p]e Builtin [P]ickers' })
 
---------------------------
+--------------------------Git Status
+vim.keymap.set('n', '<leader>gs', function()
+    builtin.git_status(themes.get_dropdown())
+end, { silent = true, noremap = true, desc = '[G]it [S]tatus' })
 
+--------------------------Git Commits
+vim.keymap.set('n', '<leader>gc', function()
+    builtin.git_commits(themes.get_ivy())
+end, { silent = true, noremap = true, desc = '[G]it [C]ommits' })
+
+--------------------------Current Buffer Commits
+vim.keymap.set('n', '<leader>gh', function()
+    builtin.git_bcommits(themes.get_dropdown())
+end, { silent = true, noremap = true, desc = "[G]it [H]istory of Current Buffer" })
+
+--------------------------Live Grep
+vim.keymap.set('n', '<leader>ps', function()
+    builtin.live_grep(themes.get_dropdown({
+        grep_open_files = false,
+        cwd = utils.buffer_dir(),
+    }))
+end, { silent = true, noremap = true, desc = '[P]arse [S]tring by Grep' })
+
+--------------------------Grep String
 vim.keymap.set('n', '<leader>pw', function()
-    builtin.grep_string({ search = vim.fn.expand('<cword>') })
-end, { desc = 'Search [p]roject for [w]ord Under Cursor' })
-
---------------------------
+    vim.ui.input({ prompt = 'Grep > ' }, function(q)
+        builtin.grep_string(themes.get_dropdown({
+            search = q,
+            grep_open_files = false,
+            cwd = utils.buffer_dir(),
+        }))
+    end)
+end, { silent = true, noremap = true, desc = 'Search [P]roject with [G]rep Under Cursor' })
 
 vim.keymap.set('n', '<leader>pW', function()
     builtin.grep_string({ search = vim.fn.expand('<cWORD>') })
-end, { desc = 'Search [P]roject for Line Under Cursor' })
+end, { silent = true, noremap = true, desc = 'Search [P]roject for c[W]ORD Under Cursor' })
 
-vim.keymap.set({ 'n', 'v' }, '<leader>pc', builtin.command_history, { desc = '[P]roject [C]ommand history' })
-vim.keymap.set({ 'n' }, '<leader>pac', builtin.autocommands, { desc = '[A]uto [c]ommands' })
--- vim.cmd('Telescope ')
--- Telescop builtins:
--- builtin.buffers 	Lists open buffers in current neovim instance
--- builtin.oldfiles 	Lists previously open files
--- builtin.commands 	Lists available plugin/user commands and runs them on <cr>
--- builtin.tags 	Lists tags in current directory with tag location file preview (users are required to run ctags -R to generate tags or update when introducing new changes)
--- builtin.command_history 	Lists commands that were executed recently, and reruns them on <cr>
--- builtin.search_history 	Lists searches that were executed recently, and reruns them on <cr>
--- builtin.help_tags 	Lists available help tags and opens a new window with the relevant help info on <cr>
--- builtin.man_pages 	Lists manpage entries, opens them in a help window on <cr>
--- builtin.marks 	Lists vim marks and their value
--- builtin.colorscheme 	Lists available colorschemes and applies them on <cr>
--- builtin.quickfix 	Lists items in the quickfix list
--- builtin.quickfixhistory 	Lists all quickfix lists in your history and open them with builtin.quickfix or quickfix window
--- builtin.loclist 	Lists items from the current window's location list
--- builtin.jumplist 	Lists Jump List entries
--- builtin.vim_options 	Lists vim options, allows you to edit the current value on <cr>
--- builtin.registers 	Lists vim registers, pastes the contents of the register on <cr>
--- builtin.autocommands 	Lists vim autocommands and goes to their declaration on <cr>
--- builtin.spell_suggest 	Lists spelling suggestions for the current word under the cursor, replaces word with selected suggestion on <cr>
--- builtin.keymaps 	Lists normal mode keymappings
--- builtin.filetypes 	Lists all available filetypes
--- builtin.highlights 	Lists all available highlights
--- builtin.current_buffer_fuzzy_find 	Live fuzzy search inside of the currently open buffer
--- builtin.current_buffer_tags 	Lists all of the tags for the currently open buffer, with a preview
--- builtin.resume 	Lists the results incl. multi-selections of the previous picker  -- Buffer history?
--- builtin.pickers 	Lists the previous pickers incl. multi-selections (see :h telescope.defaults.cache_picker)
+--------------------------Find Files
+vim.keymap.set('n', '<leader>fi', function()
+    builtin.find_files(themes.get_dropdown({
+        hidden = true,
+    }))
+end, { silent = true, noremap = true, desc = '<leader>fi Search [Fi]les' })
+
+--------------------------Git Files
+vim.keymap.set('n', '<leader>pf', function()
+    builtin.git_files(themes.get_dropdown({
+        cwd = utils.buffer_dir(),
+        layout_config = { prompt_position = 'bottom' },
+    }))
+end, { silent = true, noremap = true, desc = '<leader>pg [P]roject Git [F]iles' })
+
+--------------------------Telescope Builtin Commands
+vim.keymap.set({ 'n', 'v' }, '<leader>cm', function()
+    builtin.commands(themes.get_ivy({ winblend = 5, layout_config = { prompt_position = 'top' } }))
+end, { silent = true, noremap = true, desc = 'List All [C]o[m]mands' })
+
+vim.keymap.set({ 'n' }, '<leader>pa', function()
+    builtin.autocommands()
+end, { silent = true, noremap = true, desc = '[A]uto [c]ommands' })
+
+vim.api.nvim_create_user_command('P', function()
+    require('telescope.builtin').planets()
+end, {})
+
