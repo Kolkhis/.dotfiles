3a70a2e9622d6d3797a0db21964af42df47b1f8b
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2024-12-30 12:43:41 -0500

    wezterm: Update config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,52 +1,57 @@
-local wezterm = require('wezterm')
-local config = wezterm.config_builder()
--- https://wezfurlong.org/wezterm/config/lua/config/index.html
--- Using WSL with Wezterm:
--- https://wezfurlong.org/wezterm/config/lua/config/default_domain.html
+--[[ 
+   Wezterm configuration:
+       https://wezfurlong.org/wezterm/config/lua/config/index.html
+   Using WSL with Wezterm:
+       https://wezfurlong.org/wezterm/config/lua/config/default_domain.html
+--]]
 
-local os = wezterm.target_triple
-if os and os:match('linux') then
-    config.default_prog = { '/bin/bash' }
-elseif os and os:match('windows') then
-    config.font_dirs = { "E:/Coding/FONTS/RobotoMono", "/e/Coding/FONTS/RobotoMono" }
+local wezterm = require("wezterm")
+local mux = wezterm.mux
+local config = wezterm.config_builder()
+
+local current_os = wezterm.target_triple
+if current_os and current_os:match("linux") then
+    config.default_prog = { "/bin/bash" }
+elseif current_os and current_os:match("windows") then
+    -- config.default_domain = "WSL:Ubuntu-22.04"
     config.default_prog = { "C:/Program Files/Git/bin/bash.exe", "-i", "-l" }
-    -- config.default_prog = { "C:/msys64/usr/bin/bash.exe", "-i", "-l" }  -- MSYS2
     config.set_environment_variables = {
-        -- HOME = "/home/C", -- Use with MSYS2
         TERM = "xterm-256color",
     }
-    config.font = wezterm.font("Roboto Mono")
+elseif current_os and current_os:match("apple") then
+    config.default_prog = { "/opt/homebrew/bin/bash", "-l" }
+    config.native_macos_fullscreen_mode = true
 end
+
+config.font_dirs = { "~/wez/.fonts/" }
+config.font = wezterm.font("RobotoMono Nerd Font")
 
 config.keys = {
     {
-        key = 'F11',
+        key = "F11",
         action = wezterm.action.ToggleFullScreen,
-    }
+    },
 }
--- config.native_macos_fullscreen_mode = true
-config.window_decorations = "RESIZE"
+
+config.window_decorations = "RESIZE " -- NONE  | TITLE
 
 -- Don't show tab bar
--- config.tab_bar_at_bottom = false
-config.enable_tab_bar = false
+config.tab_bar_at_bottom = true
+config.enable_tab_bar = true --  true | false
 config.color_scheme = "Argonaut"
 
 config.visual_bell = {
-    fade_in_function = 'EaseIn',
+    fade_in_function = "EaseIn",
     fade_in_duration_ms = 150,
-    fade_out_function = 'EaseOut',
+    fade_out_function = "EaseOut",
     fade_out_duration_ms = 150,
-    target = 'CursorColor'
+    target = "CursorColor",
 }
 -- config.default_gui_startup_args = {'ssh', 'homelab'}
 
-
-local mux = wezterm.mux
-wezterm.on('gui-startup', function(cmd)
+wezterm.on("gui-startup", function(cmd)
     local tab, pane, window = mux.spawn_window(cmd or {})
     window:gui_window():set_position(200, 200)
 end)
 
 return config
-
